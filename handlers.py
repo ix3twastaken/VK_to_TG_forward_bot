ddf9da0008058bd42b953cdf05792cc6f559b58b
ix3twastaken <harkoiiika38@gmail.com>
2025-12-28 22:45:00 +0300

    Убрал лишнее, добавил комментарии

--- handlers.py
+++ handlers.py
@@ -1,33 +1,36 @@
-from telegram_bot import bot, send_photo
+from telegram_bot import *
 from vk_api import get_new_urls
 from vk_api import get_photo_urls_from_vk_post
 from db.models import *
 
 @bot.message_handler(commands=['new'])
 def send_all_new_posts(message):
-    # new_urls = get_photo_urls_from_vk_post(5)
-    # send_photo(new_urls)
-
-    new_urls, indxErr = get_new_urls()
-    if indxErr == True:
+    """
+    Отправляет все новые посты
+    """
+    new_urls, indexErr = get_new_urls()
+    if indexErr == True:
         bot.send_message(message.chat.id, "Нет новых постов")
     else:
-        send_photo(new_urls)
-    print(new_urls)
+        send_post(new_urls)
 
 @bot.message_handler(commands=['connect'])
 def connect_to_db(message):
-    urls = get_photo_urls_from_vk_post(99) 
-    print(urls)
+    """
+    Меняет значение в базе данных на последнее изображение из поста post_index
+    """
+    post_index = 10
+    urls = get_photo_urls_from_vk_post(post_index) 
     last_url = urls[-1]
     add_last_url_to_db(1, last_url)
-
-    print(last_url)
-    print(last_url_db()[0])
+    print("connected")
 
 @bot.message_handler(commands=['check'])
 def db_check(message):
-    bot.send_message(message.chat.id, f'{last_url_db()}')
+    """
+    Отправляет последний отправленный пост из базы данных
+    """
+    bot.send_photo(message.chat.id, photo=f'{last_url_db()}')
 
 
 # @bot.message_handler(commands=['other'])
